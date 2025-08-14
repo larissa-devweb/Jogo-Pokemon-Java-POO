63f42695e8c74a73a59e70938fe1848eca6348c0
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2025-08-14 10:48:14 -0300

    Update Eletrico.java

--- pokemons/Eletrico.java
+++ pokemons/Eletrico.java
@@ -2,21 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package jogopokemon.pokemons;
+package jogopokemon;
 
 import java.util.Random;
 
-public class Eletrico extends Pokemon {
+public class PokemonEletrico extends Pokemon {
     private boolean paralisou = false;
 
-    public Eletrico(String nome) {
-        super(nome, 9, 0, 1, 100, true);
+    public PokemonEletrico(String nome) {
+        super(nome,"eletrico");
     }
 
     @Override
     public int calcularDano() {
         Random rand = new Random();
         paralisou = rand.nextInt(100) < 25; // 25% de chance de paralisar
+        int forca = 0;
         return (forca + rand.nextInt(nivel + 1));
     }
 
