f5dfb9a231c746432f4ad5e994431df1f32c157d
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2025-08-14 10:49:15 -0300

    Update Terra.java

--- pokemons/Terra.java
+++ pokemons/Terra.java
@@ -2,19 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package jogopokemon.pokemons;
+package jogopokemon;
 
 import java.util.Random;
 
-public class Terra extends Pokemon {
-    public Terra(String nome) {
-        super(nome, 10, 0, 1, 100, true);
+public class PokemonTerra extends Pokemon {
+    public PokemonTerra(String nome) {
+        super(nome,"Terra");
     }
 
     
     @Override
     public int calcularDano() {
         Random rand = new Random();
+        int forca = 0;
         int dano = (forca + rand.nextInt(nivel + 1));
         if ((nivel + experiencia) % 2 != 0) {
             dano *= 2; // turno ímpar = força dobrada
