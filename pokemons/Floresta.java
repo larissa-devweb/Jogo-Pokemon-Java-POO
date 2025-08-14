193b095914106dfdb5c207de02bfdbce2ec00689
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2025-08-14 10:48:38 -0300

    Update Floresta.java

--- pokemons/Floresta.java
+++ pokemons/Floresta.java
@@ -2,18 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package jogopokemon.pokemons;
+package jogopokemon;
 
 import java.util.Random;
 
-public class Floresta extends Pokemon {
-    public Floresta(String nome) {
-        super(nome, 7, 0, 1, 100, true);
+public class PokemonFloresta extends Pokemon {
+
+    private int energia;
+    public PokemonFloresta(String nome) {
+        super(nome,"Floresta");
     }
 
     @Override
     public int calcularDano() {
         Random rand = new Random();
+        int forca = 0;
         int dano = (forca + rand.nextInt(nivel + 1));
         this.energia += 5; // regenera vida ao atacar
         if (this.energia > 100) this.energia = 100;
