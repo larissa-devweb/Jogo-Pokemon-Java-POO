d5f33872f8159ad17e76ae9fb28e010500d9c2be
Larissa <larissa.devweb@gmail.com>
2025-08-16 14:45:38 -0300

    Treinador.java completo com o método getMochila() implementado, que retorna os Pokémons do treinador como um array (como uma mochila)além de métodos para adicionar Pokémon, pontuação e posições no tabuleiro

--- Treinador.java
+++ Treinador.java
@@ -6,19 +6,60 @@ public class Treinador {
     private String nome;
     private ArrayList<Pokemon> pokemons;
     private int pontuacao;
+    private int linha, coluna;
 
     public Treinador(String nome) {
         this.nome = nome;
         this.pokemons = new ArrayList<>();
         this.pontuacao = 0;
+        this.linha = 0;
+        this.coluna = 0;
     }
 
-    public String getNome() { return nome; }
-    public ArrayList<Pokemon> getPokemons() { return pokemons; }
-    public void adicionarPokemon(Pokemon p) { pokemons.add(p); }
-    public void adicionarPontuacao(int pts) { pontuacao += pts; }
-    public int getPontuacao() { return pontuacao; }
+    // Retorna o nome do treinador
+    public String getNome() {
+        return nome;
+    }
 
+    // Retorna a lista de Pokémons do treinador
+    public ArrayList<Pokemon> getPokemons() {
+        return pokemons;
+    }
+
+    // Adiciona um Pokémon ao time
+    public void adicionarPokemon(Pokemon p) {
+        pokemons.add(p);
+        p.setTreinador(this); // associa o Pokémon a este treinador
+    }
+
+    // Adiciona pontos à pontuação do treinador
+    public void adicionarPontuacao(int pts) {
+        pontuacao += pts;
+    }
+
+    // Retorna a pontuação atual
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    // Posição do treinador no tabuleiro
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setLinha(int l) {
+        linha = l;
+    }
+
+    public void setColuna(int c) {
+        coluna = c;
+    }
+
+    // Retorna os Pokémons como um array, representando a “mochila” do treinador
     public Pokemon[] getMochila() {
         return pokemons.toArray(new Pokemon[0]);
     }
